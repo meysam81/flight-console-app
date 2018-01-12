0bfced92472ba296e321fff666fdc814b1a23955
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-12 12:52:11 +0330

    finished save; not tested yet

--- finalproject/save.cpp
+++ finalproject/save.cpp
@@ -1,7 +1,66 @@
 #include "save.h"
-
-save::save()
+template <class T>
+bool save<T>::openFile(string &fileLocation)
+{
+    saveFile.open(fileLocation, // open file from the specified location
+                  ios_base::out); // write to file; overwrite if already there
+    return saveFile.is_open();
+}
+template <class T>
+save<T>::save(bool overWrite) : overWrite(overWrite) // constructor as you may already know
 {
 
 }
+template <class T>
+bool save<T>::writeToFile(target &targetFile, vector<T> &recordVector)
+{
+    // the use of 'auto' keyword is ONLY allowed in c++11 and the following code gets the current time
+    auto currentTime = chrono::system_clock::now(); // to be used in saving file
 
+    string titleOfFile = to_string(chrono::system_clock::to_time_t(currentTime));
+    bool write = false;
+    switch (targetFile) {
+    case target::FLIGHT:
+        titleOfFile = "Flight-" + titleOfFile;
+        write = true;
+        break;
+    case target::HOST:
+        titleOfFile = "Host-" + titleOfFile;
+        write = true;
+        break;
+    case target::PASSENGER:
+        titleOfFile = "Passenger-" + titleOfFile;
+        write = true;
+        break;
+    case target::PILOT:
+        titleOfFile = "Pilot-" + titleOfFile;
+        write = true;
+        break;
+    case target::PLANE:
+        titleOfFile = "Plane-" + titleOfFile;
+        write = true;
+        break;
+    case target::TICKET:
+        titleOfFile = "Ticket-" + titleOfFile;
+        write = true;
+        break;
+    default:
+        cout << "Not a good choice!\n";
+        break;
+    }
+    if (write)
+    {
+        if (openFile(titleOfFile)) // if the file is opened
+        {
+            for (auto i : recordVector) // it's like a for_each statement
+                saveFile << i << endl; // write to file
+            saveFile.flush(); // write from buffer to disk
+            saveFile.close(); // close the file to avoid leakage
+        }
+        else
+        {
+            cout << "File not open!\n";
+            return false;
+        }
+    }
+}
--- finalproject/save.h
+++ finalproject/save.h
@@ -2,10 +2,46 @@
 #define SAVE_H
 
 
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <fstream>
+using namespace std;
+#include <vector>
+#include <chrono>
+enum class target // for different types of saves that we have
+{
+    FLIGHT = 1,
+    HOST,
+    PASSENGER,
+    PILOT,
+    PLANE,
+    TICKET
+};
+
+/* becasue we are going to insert 6 different TYPE of vectors, we should use template
+ * this allows us to use different tyes that we have in our program
+ * for example consider the following use of writeToFIle method:
+ *  * writeToFile(PILOT, vector<pilot> pilotVector), or
+ *  * writeTOFile(TICKET, vector<ticket> ticketVector)
+ *
+ *
+ * for further study please refer to the following URL:
+ * https://www.tutorialspoint.com/cplusplus/cpp_templates.htm
+ * */
+template <class T>
 class save
 {
+
+private:
+    bool overWrite;
+    fstream saveFile;
+
+    bool openFile(string &fileLocation);
 public:
-    save();
+    save(bool overWrite);
+    bool writeToFile(target &targetFile, vector<T> &recordVector);
+
 };
 
 #endif // SAVE_H
